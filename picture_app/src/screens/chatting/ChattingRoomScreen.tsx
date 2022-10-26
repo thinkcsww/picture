414c36832a93188b9678f172b16f1f63338cd6d0
sun <thinkcsw@gmail.com>
2022-10-26 23:12:08 +0900

    Ui - 채팅방 마크업 작업중

--- picture_app/src/screens/chatting/ChattingRoomScreen.tsx
+++ picture_app/src/screens/chatting/ChattingRoomScreen.tsx
@@ -3,8 +3,8 @@ import { FlatList, SafeAreaView, Text, TextInput, View } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
 import AppHeader from "../../components/AppHeader";
-import ChattingRoomListItem from "./components/ChattingRoomListItem";
 import CommonNodata from "../../components/CommonNodata";
+import { Colors } from "../../colors";
 
 const ChattingRoomScreen = () => {
   const navigation = useNavigation<any>();
@@ -13,16 +13,17 @@ const ChattingRoomScreen = () => {
     flex: 1
   }}>
     <FlatList
-      data={[1,2,3]}
-      keyExtractor={(item) => item.toString()}
-      renderItem={({ item }) => {
-        return <ChattingRoomMessageList item={item} />;
-      }}
-      showsVerticalScrollIndicator={false}
-      ListEmptyComponent={() => <CommonNodata />}
       ListHeaderComponent={() => (
         <AppHeader title={'천왕님짱'} iconName={"arrow-left"} />
       )}
+      data={[1,2,3]}
+      keyExtractor={(item) => item.toString()}
+      renderItem={({ item }) => {
+        return <ChattingRoomMessage item={item} />;
+      }}
+      ItemSeparatorComponent={() => <View style={{ height: 10 }}/>}
+      showsVerticalScrollIndicator={false}
+      ListEmptyComponent={() => <CommonNodata />}
       ListFooterComponent={() => <View style={{ height: 30}}/>}
     />
     <View style={{
@@ -42,14 +43,33 @@ type ChattingRoomMessageListProps = {
   item: any
 };
 
-const ChattingRoomMessageList = ({item}: ChattingRoomMessageListProps) => {
-  return (
-    <View style={{
-      alignSelf: 'flex-end'
-    }}>
-      <Text>hi</Text>
-    </View>
-  )
+const ChattingRoomMessage = ({item}: ChattingRoomMessageListProps) => {
+
+  if (item.sender === '') {
+    return (
+      <View style={{
+      }}>
+        <Text>hi</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={{
+        alignSelf: 'flex-end',
+        backgroundColor: Colors.PRIMARY,
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        borderRadius: 12,
+        marginRight: 12,
+        flexDirection: 'row'
+      }}>
+        <Text>오전 10:10</Text>
+        <Text style={{
+        }}>도착했습니다 도착하시면 연락주세요^^</Text>
+      </View>
+    )
+  }
+
 }
 
 export default ChattingRoomScreen;
